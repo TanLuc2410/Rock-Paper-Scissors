9366a46db35d0d3904dba682233f6b67bb9156a0
Tan Luck <149952439+TanLuc2410@users.noreply.github.com>
2024-04-15 16:20:43 +0700

    Add UI to the RPS game.
    Added 3 buttons representing player's choice.
    Added score and a function to update score.
    Reworked playRound function.
    Added Result and Score message.
    Reworked getComputerMove() because it did not work properly.

--- javascript.js
+++ javascript.js
@@ -1,58 +1,105 @@
-// Add a prompt asking for player's desired move. (make it case-insensitive)
-// Add a function that let the computer choose a move
-// Add a function that compares the moves
-// Add a function that announces that result and keep the score.
-// Create a loop that uses the previous functions to play the game 5 times and annouce the result.
+let score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+}
 
-const playerSelection = prompt("What's your move?").toLowerCase();
-const computerSelection = getComputerChoice();
+
+
+const buttons = document.createElement("div");
+buttons.classList.add("buttons");
+
+const resultDisplaying = document.createElement("div");
+resultDisplaying.classList.add("resultDisplaying");
+
+const scoreDisplaying = document.createElement("div");
+scoreDisplaying.classList.add("scoreDisplaying");
+
+
+const btn1 = document.createElement("button");
+btn1.classList.add("btn1");
+btn1.innerText = "Rock";
+const btn2 = document.createElement("button");
+btn2.classList.add("btn2");
+btn2.innerText = "Paper";
+const btn3 = document.createElement("button");
+btn3.classList.add("btn3");
+btn3.innerText = "Scissors";
+
+
+document.body.appendChild(buttons);
+buttons.appendChild(btn1);
+buttons.appendChild(btn2);
+buttons.appendChild(btn3);
+document.body.appendChild(resultDisplaying);
+document.body.appendChild(scoreDisplaying);
+resultText = document.querySelector(".resultDisplaying");
+
+
+buttons.addEventListener("click", (event) => {
+    if (event.target.className === "btn1") {
+            playRound("rock");
+    } else if (event.target.className === "btn2") {               
+            playRound("paper");
+    } else {
+            playRound("scissors");
+    }
+        
+    });
+
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+    let result = "";
+    
+    if (playerSelection === computerSelection) {
+        result = `It's a tie.`;
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+                (playerSelection === "paper" && computerSelection === "rock") ||
+                (playerSelection === "scissors" && computerSelection === "paper")) {
+        result = `It's your win!`;
+    } else {
+        result = `It's your loss!`;
+    }
+
+    if (result === "It's a tie.") {
+        score.ties++;
+    } else if (result === "It's your win!") {
+        score.wins++;
+    } else {
+        score.losses++;
+    }
+
+    updateScore();
+
+    resultText.textContent = `You chose ${playerSelection}. Computer picked ${computerSelection}. ${result}`;
+
+}
+
+function updateScore() {
+    document.querySelector(".scoreDisplaying").textContent = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+
+    if (score.wins === 5) {
+        document.querySelector(".scoreDisplaying").textContent = `You won 5 rounds. You are the winner!`;
+    } else if (score.losses === 5) {
+        document.querySelector(".scoreDisplaying").textContent = "You lost 5 rounds. The Computer is the winner!";
+    }
+}
+
+
 
 
 function getComputerChoice() {
-    const computerMove =  Math.random();
-
-    if (computerMove >= 0 || computerMove < 1/3) {
-        return "rock";
-    } else if (computerMove >= 1/3 || computerMove < 2/3) {
-        return "paper";
+    const randomNumber =  Math.random();
+    let computerMove = "";
+     
+    if (randomNumber >= 0 && randomNumber < 1/3) {
+        computerMove = "rock";
+    } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+        computerMove = "paper";
     } else {
-        return "scissors";
+        computerMove = "scissors";
     }
+
+    return computerMove;
 }
 
-function playRound(playerSelection, computerSelection) {
-    
-
-    if (playerSelection === "rock") {
-        if(computerSelection === "rock") {
-        return `You chose ${playerSelection}, the computer picked ${computerSelection} as well. It's a tie.`;
-        } else if (computerSelection === "paper") {
-        return `You chose rock, the computer picked paper. It's your loss.`;
-        } else {
-        return `You chose rock, the computer picked scissors. It's your win.`;
-        }
-    }
-
-    if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-        return `You chose paper, the computer picked rock. It's your win.`;
-        } else if (computerSelection === "paper") {
-        return `You chose paper, the computer picked paper as well. It's a tie.`;
-        } else {
-        return `You chose paper, the computer picked scissors. It's your loss.`;
-        }
-    }
-    if (playerSelection === "scissor" && playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-        return `You chose scissors, the computer picked rock. It's your loss`;
-    } else if (computerSelection === "paper") {
-        return `You chose scissors, the computer picked paper. It's your win.`;
-    } else {
-        return `You chose scissors, the computer picked scissors as well. It's a tie.`;
-        }
-    }
-    
-}
-
-
-console.log(playRound(playerSelection, computerSelection)); 
